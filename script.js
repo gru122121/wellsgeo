ad9397d0efa0bc3370cd497ad59df6ffc1172f32
squidward1.1 <90065237+gru122121@users.noreply.github.com>
2024-12-11 10:15:58 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,15 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Movies section not found');
         }
     };
+
+    document.addEventListener('scroll', () => {
+        const header = document.querySelector('header');
+        if (window.scrollY > 10) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
 }); 
 
 
